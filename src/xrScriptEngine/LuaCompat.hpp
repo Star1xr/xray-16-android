7187bf6cc44653dec2e6a5133f193c113d91137b
Star1xr <star1xx77ff@gmail.com>
2026-09-12 19:54:25 +0300

    Fix luaJIT_profile_callback typedef signature

--- src/xrScriptEngine/LuaCompat.hpp
+++ src/xrScriptEngine/LuaCompat.hpp
@@ -5,11 +5,11 @@
 #endif
 
 #ifndef XRAY_USE_LUAJIT
-typedef void (*luaJIT_profile_callback)(lua_State* L, void* data);
+typedef void (*luaJIT_profile_callback)(void* data, lua_State* L, int samples, int vmstate);
 #define LUA_BITLIBNAME "bit"
 #define LUA_FFILIBNAME "ffi"
 #define LUA_JITLIBNAME "jit"
-inline void  luaopen_bit(lua_State*) {}
-inline void  luaopen_ffi(lua_State*) {}
-inline void  luaopen_jit(lua_State*) {}
+inline void luaopen_bit(lua_State*) {}
+inline void luaopen_ffi(lua_State*) {}
+inline void luaopen_jit(lua_State*) {}
 #endif
